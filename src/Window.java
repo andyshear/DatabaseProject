e2eb9ddae53367a25a69bd5cb1558a0e7ab5d20e
Brandon Dadosky <dadoskbm@miamioh.edu>
2014-04-24 20:30:17 -0400

    Updated GUI

--- src/Window.java
+++ src/Window.java
@@ -11,6 +11,7 @@ import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
+import java.awt.Font;
 
 
 public class Window {
@@ -18,7 +19,6 @@ public class Window {
 	private JFrame frmTeamMonstarsProject;
 	private JTextField moodText;
 	private JTextField activityText;
-	private JTextField nameText;
 
 	/**
 	 * Launch the application.
@@ -57,12 +57,14 @@ public class Window {
 		
 		moodText = new JTextField();
 		springLayout.putConstraint(SpringLayout.WEST, moodText, 23, SpringLayout.WEST, frmTeamMonstarsProject.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, moodText, -522, SpringLayout.SOUTH, frmTeamMonstarsProject.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, moodText, -129, SpringLayout.EAST, frmTeamMonstarsProject.getContentPane());
 		frmTeamMonstarsProject.getContentPane().add(moodText);
 		moodText.setColumns(10);
 		
 		JButton btnSearchMood = new JButton("Search");
-		springLayout.putConstraint(SpringLayout.EAST, moodText, -6, SpringLayout.WEST, btnSearchMood);
-		springLayout.putConstraint(SpringLayout.WEST, btnSearchMood, 322, SpringLayout.WEST, frmTeamMonstarsProject.getContentPane());
+		springLayout.putConstraint(SpringLayout.NORTH, btnSearchMood, 32, SpringLayout.NORTH, frmTeamMonstarsProject.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, btnSearchMood, 6, SpringLayout.EAST, moodText);
 		springLayout.putConstraint(SpringLayout.EAST, btnSearchMood, -10, SpringLayout.EAST, frmTeamMonstarsProject.getContentPane());
 		btnSearchMood.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -71,37 +73,36 @@ public class Window {
 		frmTeamMonstarsProject.getContentPane().add(btnSearchMood);
 		
 		JLabel lblYourMood = new JLabel("Your mood:");
-		lblYourMood.setToolTipText("Enter your preferred moods, separated by commas");
 		springLayout.putConstraint(SpringLayout.NORTH, moodText, 6, SpringLayout.SOUTH, lblYourMood);
-		springLayout.putConstraint(SpringLayout.SOUTH, moodText, 29, SpringLayout.SOUTH, lblYourMood);
 		springLayout.putConstraint(SpringLayout.WEST, lblYourMood, 23, SpringLayout.WEST, frmTeamMonstarsProject.getContentPane());
+		springLayout.putConstraint(SpringLayout.NORTH, lblYourMood, 10, SpringLayout.NORTH, frmTeamMonstarsProject.getContentPane());
+		lblYourMood.setToolTipText("Enter your preferred moods, separated by commas");
 		frmTeamMonstarsProject.getContentPane().add(lblYourMood);
 		
 		JLabel lblYourActivity = new JLabel("Your activity:");
+		springLayout.putConstraint(SpringLayout.NORTH, lblYourActivity, 1, SpringLayout.SOUTH, moodText);
+		springLayout.putConstraint(SpringLayout.WEST, lblYourActivity, 23, SpringLayout.WEST, frmTeamMonstarsProject.getContentPane());
 		lblYourActivity.setToolTipText("Enter your preferred activities, separated by commas");
-		springLayout.putConstraint(SpringLayout.NORTH, lblYourActivity, 6, SpringLayout.SOUTH, moodText);
-		springLayout.putConstraint(SpringLayout.WEST, lblYourActivity, 0, SpringLayout.WEST, moodText);
 		frmTeamMonstarsProject.getContentPane().add(lblYourActivity);
 		
 		activityText = new JTextField();
 		springLayout.putConstraint(SpringLayout.NORTH, activityText, 6, SpringLayout.SOUTH, lblYourActivity);
 		springLayout.putConstraint(SpringLayout.WEST, activityText, 23, SpringLayout.WEST, frmTeamMonstarsProject.getContentPane());
-		springLayout.putConstraint(SpringLayout.SOUTH, activityText, 29, SpringLayout.SOUTH, lblYourActivity);
-		springLayout.putConstraint(SpringLayout.EAST, activityText, 0, SpringLayout.EAST, moodText);
+		springLayout.putConstraint(SpringLayout.SOUTH, activityText, -473, SpringLayout.SOUTH, frmTeamMonstarsProject.getContentPane());
 		frmTeamMonstarsProject.getContentPane().add(activityText);
 		activityText.setColumns(10);
 		
 		JButton btnSearchActivity = new JButton("Search");
-		springLayout.putConstraint(SpringLayout.NORTH, btnSearchActivity, 128, SpringLayout.NORTH, frmTeamMonstarsProject.getContentPane());
-		springLayout.putConstraint(SpringLayout.SOUTH, btnSearchMood, -26, SpringLayout.NORTH, btnSearchActivity);
-		springLayout.putConstraint(SpringLayout.WEST, btnSearchActivity, 322, SpringLayout.WEST, frmTeamMonstarsProject.getContentPane());
-		springLayout.putConstraint(SpringLayout.EAST, btnSearchActivity, 0, SpringLayout.EAST, btnSearchMood);
+		springLayout.putConstraint(SpringLayout.NORTH, btnSearchActivity, 23, SpringLayout.SOUTH, btnSearchMood);
+		springLayout.putConstraint(SpringLayout.EAST, activityText, -6, SpringLayout.WEST, btnSearchActivity);
+		springLayout.putConstraint(SpringLayout.WEST, btnSearchActivity, 0, SpringLayout.WEST, btnSearchMood);
+		springLayout.putConstraint(SpringLayout.EAST, btnSearchActivity, -10, SpringLayout.EAST, frmTeamMonstarsProject.getContentPane());
 		frmTeamMonstarsProject.getContentPane().add(btnSearchActivity);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		springLayout.putConstraint(SpringLayout.WEST, scrollPane, 20, SpringLayout.WEST, frmTeamMonstarsProject.getContentPane());
-		springLayout.putConstraint(SpringLayout.SOUTH, scrollPane, -36, SpringLayout.SOUTH, frmTeamMonstarsProject.getContentPane());
-		springLayout.putConstraint(SpringLayout.EAST, scrollPane, 415, SpringLayout.WEST, frmTeamMonstarsProject.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, scrollPane, 23, SpringLayout.WEST, frmTeamMonstarsProject.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, scrollPane, -95, SpringLayout.SOUTH, frmTeamMonstarsProject.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, scrollPane, -27, SpringLayout.EAST, frmTeamMonstarsProject.getContentPane());
 		frmTeamMonstarsProject.getContentPane().add(scrollPane);
 		
 		JList list = new JList();
@@ -117,32 +118,16 @@ public class Window {
 		scrollPane.setViewportView(list);
 		
 		JLabel lblYouShouldListen = new JLabel("You should listen to:");
+		springLayout.putConstraint(SpringLayout.NORTH, lblYouShouldListen, 20, SpringLayout.SOUTH, activityText);
 		springLayout.putConstraint(SpringLayout.WEST, lblYouShouldListen, 23, SpringLayout.WEST, frmTeamMonstarsProject.getContentPane());
-		springLayout.putConstraint(SpringLayout.SOUTH, lblYouShouldListen, -380, SpringLayout.SOUTH, frmTeamMonstarsProject.getContentPane());
 		springLayout.putConstraint(SpringLayout.NORTH, scrollPane, 6, SpringLayout.SOUTH, lblYouShouldListen);
 		frmTeamMonstarsProject.getContentPane().add(lblYouShouldListen);
 		
-		JLabel lblYourName = new JLabel("Your name:");
-		lblYourName.setToolTipText("Enter your name");
-		springLayout.putConstraint(SpringLayout.NORTH, lblYourName, 10, SpringLayout.NORTH, frmTeamMonstarsProject.getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST, lblYourName, 23, SpringLayout.WEST, frmTeamMonstarsProject.getContentPane());
-		frmTeamMonstarsProject.getContentPane().add(lblYourName);
-		
-		nameText = new JTextField();
-		springLayout.putConstraint(SpringLayout.WEST, nameText, 23, SpringLayout.WEST, frmTeamMonstarsProject.getContentPane());
-		springLayout.putConstraint(SpringLayout.NORTH, lblYourMood, 6, SpringLayout.SOUTH, nameText);
-		springLayout.putConstraint(SpringLayout.NORTH, nameText, 6, SpringLayout.SOUTH, lblYourName);
-		springLayout.putConstraint(SpringLayout.SOUTH, nameText, 29, SpringLayout.SOUTH, lblYourName);
-		frmTeamMonstarsProject.getContentPane().add(nameText);
-		nameText.setColumns(10);
-		
-		JButton btnUserInfo = new JButton("User Info");
-		springLayout.putConstraint(SpringLayout.EAST, nameText, -6, SpringLayout.WEST, btnUserInfo);
-		springLayout.putConstraint(SpringLayout.NORTH, btnUserInfo, -550, SpringLayout.SOUTH, frmTeamMonstarsProject.getContentPane());
-		springLayout.putConstraint(SpringLayout.SOUTH, btnUserInfo, -527, SpringLayout.SOUTH, frmTeamMonstarsProject.getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST, btnUserInfo, 0, SpringLayout.WEST, btnSearchMood);
-		springLayout.putConstraint(SpringLayout.EAST, btnUserInfo, 0, SpringLayout.EAST, btnSearchMood);
-		frmTeamMonstarsProject.getContentPane().add(btnUserInfo);
+		JLabel lblTimeListenedTo = new JLabel("Time listened to: 0 minutes");
+		springLayout.putConstraint(SpringLayout.WEST, lblTimeListenedTo, 23, SpringLayout.WEST, frmTeamMonstarsProject.getContentPane());
+		lblTimeListenedTo.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		springLayout.putConstraint(SpringLayout.NORTH, lblTimeListenedTo, 17, SpringLayout.SOUTH, scrollPane);
+		frmTeamMonstarsProject.getContentPane().add(lblTimeListenedTo);
 	}
 
 	private class SwingAction extends AbstractAction {
